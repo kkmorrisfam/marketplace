70e2945e7cd33c91c2f9451d2835ded04e270eed
Kerri Morris <kkmorrisfam@hotmail.com>
2026-02-17 20:16:55 -0800

    add shadcn packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (<div>
-    <h1>Welcome to start page.</h1>
+    <h1 className="font-bold text-blue-500 font-barlow">Welcome to start page.</h1>
+    <Button>Click here</Button>
   </div>
   );
 }
