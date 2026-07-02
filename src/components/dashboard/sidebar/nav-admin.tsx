3b05d70346803c027735d231c94ee4d23cb03141
Kerri Morris <kkmorrisfam@hotmail.com>
2026-07-02 16:48:34 -0700

    Add icons for admin nav bar to page

---
+++ src/components/dashboard/sidebar/nav-admin.tsx
@@ -0,0 +1,46 @@
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { icons } from "@/constants/icons";
+import { DashboardSidebarMenuInterface } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export default function SidebarNavAdmin({
+    menuLinks,
+  }: {
+    menuLinks:DashboardSidebarMenuInterface[];
+  }) {
+    return (
+      <nav className="relative grow">
+        <Command className="rounded-lg overflow-visible bg-transparent">
+            <CommandInput placeholder="Search..."/>
+            <CommandList className="py-2 overflow-visible">
+                <CommandEmpty>No Links Found.</CommandEmpty>
+                <CommandGroup className="overflow-visable pt-0 relative">
+                    {menuLinks.map((link, index)=>{
+                        let icon;
+                        const iconSearch = icons.find((icon)=>icon.value===link.icon);
+                        if(iconSearch) icon = <iconSearch.path />;
+                        return (
+                            <CommandItem 
+                              key={index}
+                              className={cn("w-full h-12 cursor-pointer mt-1")}
+                            >
+                            <Link 
+                              href={link.link} 
+                              className="flex items-center gap-2 hover:bg-transparent rounded-md transition-all w-full"
+                            >
+                                {icon}
+                                <span>{link.label}</span>
+                            </Link>
+                            </CommandItem>
+                            )
+                    })
+
+                    }
+                </CommandGroup>
+            </CommandList>
+            
+        </Command>
+      </nav>
+    );
+}
--- src/components/dashboard/sidebar/sidebar.tsx
+++ src/components/dashboard/sidebar/sidebar.tsx
@@ -2,6 +2,8 @@ import Logo from "@/components/shared/logo";
 //import { FC } from "react";
 import UserInfo from "./user-info";
 import { User } from "@/generated/prisma/client";
+import SidebarNavAdmin from "./nav-admin";
+import { adminDashboardSidebarOptions } from "@/constants/data";
 
 interface SidebarProps{
     isAdmin?:boolean;
@@ -12,10 +14,13 @@ export default function Sidebar({ isAdmin, user }: SidebarProps) {
     
     
     //get the user object/current User
-    return <div className="w-75 border-r h-screen p-4 flex flex-col fixed top-0 left-0 bottom-0">
-       <Logo width="100%" height="180px"/>
-       <span className="mt-3" />
-       <UserInfo user={user} />
-    </div>;
+    return (
+       <div className="w-75 border-r h-screen p-4 flex flex-col fixed top-0 left-0 bottom-0">
+         <Logo width="100%" height="180px"/>
+         <span className="mt-3" />
+         <UserInfo user={user} />
+         {isAdmin && <SidebarNavAdmin menuLinks={adminDashboardSidebarOptions} />}
+        </div>
+    );
 }
 
